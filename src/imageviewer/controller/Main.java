661226afbd72de94f6797d7d0701699b08ad023e
nahimaort <nahimaortega@gmail.com>
2021-12-28 10:17:10 +0000

    ImageViewer versión 1

---
+++ src/imageviewer/controller/Main.java
@@ -0,0 +1,16 @@
+package imageviewer.controller;
+
+import imageviewer.model.Image;
+import imageviewer.persistence.FileImageLoader;
+import java.io.File;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        File file = new File("Images");
+        FileImageLoader imageLoader = new FileImageLoader(file);
+        Image image = imageLoader.load();
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.getImageDisplay().show(image);
+    }
+}
---
+++ src/imageviewer/ui/ImageDisplay.java
@@ -0,0 +1,8 @@
+package imageviewer.ui;
+
+import imageviewer.model.Image;
+
+public interface ImageDisplay {
+    Image current();
+    void show(Image image);
+}
